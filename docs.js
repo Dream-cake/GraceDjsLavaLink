1957873390b1f0567b4661fceb82f7ed7d58b733
Dream-cake <thespider84@gmail.com>
2020-03-01 16:40:37 -0500

    start

---
+++ docs.js
@@ -0,0 +1,56 @@
+const Docma = require("docma");
+const Package = require("./package");
+
+Docma.create()
+    .build({
+        app: {
+            title: Package.name,
+            base: "/GraceDjsLavaLink",
+            entrance: "content:readme",
+            routing: "query",
+            server: Docma.ServerType.GITHUB
+        },
+        markdown: {
+            gfm: true,
+            tables: true,
+            breaks: true,
+            pedantic: true,
+            sanitize: true,
+            smartLists: true,
+            smartypants: true,
+            tasks: true,
+            emoji: true
+        },
+        src: [
+            { readme: "./README.md" },
+            { lavalink: "./src/*/**/*.js" }
+        ],
+        dest: "./docs",
+        jsdoc: { plugins: ["jsdoc-dynamic"] },
+        template: {
+            options: {
+                title: Package.name,
+                navItems: [
+                    {
+                        label: "Readme",
+                        href: "?content=readme"
+                    },
+                    {
+                        label: "Documentation",
+                        href: "?api=lavalink",
+                        iconClass: "ico-book"
+                    },
+                    {
+                        label: "GitHub",
+                        href: Package.homepage,
+                        target: "_blank",
+                        iconClass: "ico-md ico-github"
+                    }
+                ],
+                navbar: {
+                    dark: true
+                }
+            }
+        }
+    })
+    .catch(console.error);
